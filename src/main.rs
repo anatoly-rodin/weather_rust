83702fcea5a7271519573ab57605808b3c9a8e17
an.rodin <anatoly.richman@gmail.com>
2026-07-01 14:08:53 +0300

    Added function to get the weather data from API.

--- src/main.rs
+++ src/main.rs
@@ -32,3 +32,16 @@ struct Main {
 struct Wind {
     speed: f64,
 }
+
+/// Function to get the weather information from OpenWeatherMap API.
+fn get_weather(city: &str, country_code: &str, api_key: &str) -> Result<WeatherResponse, reqwest::Error> {
+    let url: String = format!(
+        "http://api.openweathermap.org/data/2.5/weather?q={},{}&units=metric&appid={}",
+        city, country_code, api_key,
+    );
+
+    let response = reqwest::blocking::get(&url)?;
+    let response_json = response.json::<WeatherResponse>()?;
+
+    Ok(response_json)
+}
